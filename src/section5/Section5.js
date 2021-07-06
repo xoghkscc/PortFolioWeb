3579eaa940405906f41e611b2ea5172328076221
xoghkscc <82793713+xoghkscc@users.noreply.github.com>
2021-07-06 23:57:08 +0900

    1-1차 완성

--- src/section5/Section5.js
+++ src/section5/Section5.js
@@ -4,14 +4,26 @@ import emailjs from 'emailjs-com';
 import './section5.css';
 export default function Section5() {
     function sendEmail(e) {
-
+        e.preventDefault();
+        const resetvalue = document.getElementsByClassName("inputtext");
+        
         emailjs.sendForm('service_387vost', 'template_1zo0coz', e.target, 'user_hTV6HhgKIxl6ivbBaCMob')
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
-            });
+        .then((result) => {
+            console.log(result.text);
+            alert(`메일을 성공적으로 보냈습니다`);
+        }, (error) => {
+            console.log(error.text);
+            alert(`메일보내기 실패하였습니다`);
+        });
+        
+        Array.from(resetvalue).forEach((value)=>{
+            value.value = '';
+        })
+
     }
+
+    // function inputreset() {
+    // }
     return (
         <article >
             <div class="contact">
@@ -23,17 +35,17 @@ export default function Section5() {
                         <div className="Adress myinfo">e-mail : xoghkscc@naver.com</div>
                         <div className="Adress myinfo">(이메일은 자주 확인합니다^^)</div>
                     </div>
-                   
+
                     <div className="rightcontactPanel sub2">
                         <form className="contactform " onSubmit={sendEmail}>
                             <input type="hidden" name="contact_number" required autoComplete='off' />
                             <label>이름</label>
-                            <input className="inputclass" type="text" name="user_name" required autoComplete='off' />
+                            <input className="inputtext inputclass" type="text" name="user_name" required autoComplete='off' />
                             <label>이메일</label>
-                            <input className="inputclass" type="email" name="user_email" required autoComplete='off' />
+                            <input className="inputtext inputclass" type="email" name="user_email" required autoComplete='off' />
                             <label>메세지 입력</label>
-                            <textarea className="inputclass2" name="message" />
-                            <input className="sumitbutton" type="submit" value="보내기" required autoComplete='off' />
+                            <textarea className="inputtext inputclass2" name="message" />
+                            <input className="sumitbutton" type="submit" value="보내기" required autoComplete='off' onClick="inputreset()" />
                         </form>
                     </div>
                 </div>
