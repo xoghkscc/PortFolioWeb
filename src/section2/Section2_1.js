fdf89b613549e06afe2d8b5e919e314a423ff3e3
xoghkscc <82793713+xoghkscc@users.noreply.github.com>
2021-07-20 00:08:16 +0900

    21.07.20

--- src/section2/Section2_1.js
+++ src/section2/Section2_1.js
@@ -19,11 +19,38 @@ class Section2_1 extends Component {
                             </span>
                         </div>
                         <div class="text whoiam ">About Me?</div>
-                        <div class="text info ">내용 적는 곳</div>
+                        <div class="text info ">
+                            <div class="info1">
+                            <img src={process.env.PUBLIC_URL + '/profile.jpg'} class="profileImg"></img>
+                            </div>
+                            <div class="info2">
+                                <div>
+                                    <dl>
+                                        <dt>
+                                            <div class="block">
+                                                <div class="miniblock">History</div>
+                                            </div>
+                                        </dt>
+                                        <dd>- 2012. 03.01 ~ 2018. 02. 15 신라대학교 수학교육과 졸업</dd>
+                                        <dd>- 2021. 03.15 ~ 2021. 09. 07 KG아이디뱅크 백엔드 과정 수료</dd>
+                                    </dl>
+                                </div>
+                                <div>
+                                    <dl>
+                                        <dt>
+                                            <div class="block">
+                                                <div class="miniblock">MySelf</div>
+                                            </div>
+                                        </dt>
+                                        <dd>안녕하세요. </dd>
+                                    </dl>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </article>
-                )
+        )
     }
 }
-                export default Section2_1;
+export default Section2_1;
